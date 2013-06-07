4172ef18f581cefa1b1afdde08129503ed700f02
johnny-ray <johnny@johnnyray.me>
2013-06-07 11:29:35 -0400

    changes

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,44 +7,95 @@ module
     Utils.$scope = $scope;
     Utils.$http = $http;
     $scope.new_member_name = "";
+    $scope.new_member_secret_identity = "";
     $scope.new_member_abilities = [];
     $scope.new_ability = "";    
 
-    $scope.members = [
-      // { 
-      //   name: "Superman",
-      //   secret_identity: "Clark Kent",
-      //   powers: [ "super strength", "flight", "heat vision", "speed", "x-ray vision" ]
-      // },
-      // {
-      //   name: "Batman",
-      //   secret_identity: "Bruce Wayne",
-      //   powers: []
-      // }
-    ];
+    $scope.members = [];
+
+    $scope.safeApply = function(fn) {
+      var phase = this.$root.$$phase;
+      if(phase == '$apply' || phase == '$digest') {
+        if(fn && (typeof(fn) === 'function')) {
+          fn();
+        }
+      } else {
+        this.$apply(fn);
+      }
+    };
 
     $scope.noMembers = function() {
       return $scope.members.length === 0;
     };
 
+    $scope.removeHero = function(id) {
+
+    };
+
     $scope.removeAbility = function(ability) {
-      console.log("remoe " + $socability)
+      var index = $scope.new_member_abilities.indexOf( ability );
+      if ( index > -1 ) {
+        $scope.new_member_abilities.splice( index, 1 );
+      }
     };
 
     $scope.addAbility = function() {
-      console.log("add " + $scope.new_member_ability)
+      if ( $scope.new_member_abilities.indexOf( $scope.new_member_ability ) === -1  && $scope.new_member_ability !== "" ) {
+        $scope.new_member_abilities.push( $scope.new_member_ability );
+        $scope.new_member_ability = ""
+      }
     }
 
-    $scope.saveHero = function() {
+    $scope.saveHero = Utils.saveHero.bind( Utils );
 
+    $scope.addMember = function(hero) {
+      console.log(arguments)
     }
+
+    $scope.onHeroes = function(heroes) {
+      $scope.members = heroes;
+      console.log(heroes)
+    }
+
+    Utils.getMembers();
   
   });
 
 var Utils = {
 
   getMembers: function() {
-    var url = "/heroes/1.json"
+    var self = this;
+    var url = "/heroes.json"
+    this.$http({
+      method: "GET",
+      url: url
+    })
+
+    .error( function(){ console.error(argumetns)} )
+
+    .success( self.$scope.onHeroes )
+  },
+
+  saveHero: function() {
+    var $scope = this.$scope;
+    var $http = this.$http;
+
+    var data = {
+      name: $scope.new_member_name,
+      secret_identity: $scope.new_member_secret_identity,
+      abilities: $scope.new_member_abilities
+    };
+
+    $http({
+      method: "POST",
+      url: "/heroes.json",
+      data: data
+    })
+
+    .error( function(){ console.error("an error"); console.error(arguments) } )
+
+    .success( $scope.addMember )
+
   }
 
 };
