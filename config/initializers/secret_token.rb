b71111d94191553bb019051e38334b639f0686d4
Colin Van Dyke <colin@everfi.com>
2013-06-07 09:25:38 -0400

    MMMMMmmmmmm

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Heroes::Application.config.secret_key_base = '359143cbcae832c5754fe3fb328a1f093d5a302f68f074ea08249295770e2328d813bb25fb6adefdf4d605aedcc69f4c4665e678784f129eaa816c728654da7c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Heroes::Application.config.session_store :cookie_store, key: '_heroes_session'
